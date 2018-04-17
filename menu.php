e09133d211359912b127263f193c1fafc48cc5f4
stebau91 <31169217+stebau91@users.noreply.github.com>
2018-04-17 17:21:45 +0100

    Delete menu.php

--- menu.php
+++
@@ -1,9 +0,0 @@
-
-<ul>
-    <?php
-        echo "<li><a href='index.php'>Home</a></li>";
-        echo "<li><a href='#'>University</a></li>";
-        echo "<li><a href='#'>About</a></li>";
-        echo "<li><a href='#'>Contact</a></li>";
-    ?>
-</ul>
