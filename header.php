45ad352b6e92c55e6074065f5eb1c73acbd0fee5
stebau91 <31169217+stebau91@users.noreply.github.com>
2018-04-17 17:21:25 +0100

    Delete header.php

--- header.php
+++
@@ -1,8 +0,0 @@
-<?php    
-    
-if (stripos($_SERVER['REQUEST_URI'], 'index.php' )){
-     echo "<li><a href='adminlogin.php'>Administrator</a></li>
-           <li><a href='intranetlogin.php'>Intranet for members</a></li>";
-}
-
-?>
